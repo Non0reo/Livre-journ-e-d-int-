b6286bf681e43cf82d2f24b497a13ec1cd680c30
Non0reo <pieroni.nolhan@gmail.com>
2024-09-02 23:02:50 +0200

    Fix Images

--- sketch.js
+++ sketch.js
@@ -10,27 +10,32 @@ let tex1;
 let tex2;
 
 function preload() {
-  tex0 = loadImage('/assets/totoro.jpg');
-  tex1 = loadImage('/assets/lego.png');
-  tex2 = loadImage('/assets/dog.jpg');
+  tex0 = loadImage('assets/totoro.jpg');
+  tex1 = loadImage('assets/dog.jpg');
+  tex2 = loadImage('assets/lego.png');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
+function animationClamped(start, end) {
+  return constrain(map(mouseX, 100, width - 100, start, end), start, end);
+}
+
 function draw() {
   background(30);
   angle = constrain(map(mouseX, 100, width - 100, 0, -PI), -PI, 0);
-  perspective = constrain(map(mouseX, 100, width - 100, 0.7, 0.9), 0.7, 0.9);
-  bookHeight = constrain(map(mouseX, 100, width - 100, 140, 190), 140, 190);
+  perspective = animationClamped(0.7, 0.9);
+  bookHeight = animationClamped(140, 190);
 
   textureMode(NORMAL);
   scale(3.0);
   stroke(0);
   strokeWeight(30);
+  //noStroke();
 
-  texture(tex1);
+  texture(tex2);
   //rect(bookPosX, bookPosY, bookPageLength, bookHeight);
   beginShape();
   vertex(bookPosX, bookPosY, 0, 0);
@@ -39,8 +44,7 @@ function draw() {
   vertex(bookPosX, bookPosY + bookHeight, 0, 1);
   endShape(CLOSE);
 
-
-  texture(angle > -PI / 2 ? tex0 : tex2);
+  texture(angle > -PI / 2 ? tex0 : tex1);
   beginShape();
   vertex(bookPosX, bookPosY, 0, 0);
   vertex(cos(angle) * bookPageLength * perspective  + bookPosX, sin(angle) * bookPageLength + bookPosY, 1, 0);
